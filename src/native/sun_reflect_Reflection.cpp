91f871bae032db9d271eab0b436ee5bb96d835a6
newNcy <1136962813@qq.com>
2020-04-30 15:17:02 -0400

    将getCallerClass的语义从'返回调用getCallerClass的函数所属的类'改成'返回调用getCallerClass的方法的调用者类'

--- src/native/sun_reflect_Reflection.cpp
+++ src/native/sun_reflect_Reflection.cpp
@@ -4,7 +4,7 @@
 
 NATIVE jreference sun_reflect_Reflection_getCallerClass(environment * env, jreference cls,  jreference)
 {
-	return env->get_thread()->current_frame->caller_frame->current_class->mirror;
+	return env->get_thread()->current_frame->caller_frame->caller_frame->current_class->mirror;
 }
 
 NATIVE jint sun_reflect_Reflection_getClassAccessFlags(environment * env, jreference cls)
